a9bf8bce4878b27c8e9373848fc2ca675761bd33
smurve <wgiersche@gmail.com>
2018-11-02 08:10:13 +0000

    Zwischenstand

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="dataflow_pipeline_dependencies",
+    version="1.0.0",
+    author="Wolfgang Giersche",
+    author_email="wgiersche@gmail.com",
+    description=("Custom python utils needed for dataflow cloud runner"),
+    packages=[
+        'utilities'
+        ]
+)
